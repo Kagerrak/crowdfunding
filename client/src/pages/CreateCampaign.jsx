1a87e7a00583778310cca60c07eaddb9601fa0df
Kage <romzhey@gmail.com>
2023-01-17 23:28:25 +0800

    frontend update

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ethers } from "ethers";
 
-import { money } from "../assets";
+import { money, loader } from "../assets";
 import { CustomButton, FormField } from "../components";
 import { checkIfImage } from "../utils";
 import { useStateContext } from "../context";
@@ -44,7 +44,13 @@ const CreateCampaign = () => {
   };
   return (
     <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
-      {isLoading && "Loader.."}
+      {isLoading && (
+        <img
+          src={loader}
+          alt="loader"
+          className="w-[100px] h-[100px] object-contain"
+        />
+      )}
       <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3A3A43] rounded-[10px]">
         <h1 className="font-epilogue font-bold sm:text[25px] text-[18px] leading-[38px] text-white">
           Start a Campaign
